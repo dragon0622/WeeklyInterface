ccad572a94b839add8ba252e8d1ed869cf6f2fd9
dragon0622 <iamdragon.dev@gmail.com>
2024-02-03 00:47:59 +0900

    Feat : 매직넘버 수정

---
+++ src/MagicNum.java
@@ -0,0 +1,12 @@
+public class MagicNum {
+    public static final int OVER_WEIGHT = 10;
+    public static final int LIGHT_WEIGHT_CUT = 3;
+    public static final int OVER_WEIGHT_TAX = 10000;
+    public static final int MIDDLE_WEIGHT_TAX = 5000;
+    public static final int LIGHT_WEIGHT_TAX = 1000;
+
+    public static final int FREE_DELIVERY_FEE_CUT = 100000;
+    public static final int DISCOUNT_DELIVERY_FEE_CUT = 30000;
+    public static final int DISCOUNT_FEE = 1000;
+
+}
