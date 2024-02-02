e4f81c0a83e60743e3d8838896bed02cc19989c2
dragon0622 <iamdragon.dev@gmail.com>
2024-02-03 00:49:09 +0900

    Feat : 함수 재정의 및 출력함수 추가

--- src/Beauty.java
+++ src/Beauty.java
@@ -1,56 +1,17 @@
 public class Beauty extends Product implements DeliveryChargeCalculator{
 
-    final int OVER_WEIGHT = 10;
-    final int LIGHT_WEIGHT_CUT = 3;
-    final int OVER_WEIGHT_TAX = 10000;
-    final int MIDDLE_WEIGHT_TAX = 5000;
-    final int LIGHT_WEIGHT_TAX = 1000;
-
-    final int FREE_DELIVERY_PEE_CUT = 100000;
-    final int DISCOUNT_DELIVERY_PEE_CUT = 30000;
-    final int DISCOUNT_PEE = 1000;
-
-    private int weight;
-    private int price;
-
-
+    Beauty(String name, int price, int weight){
+        super(name, price, weight);
+    }
 
     @Override
-    public int getDeliveryCharge(int weight, int price) {
-        int charge = 0;
-        if (weight >= OVER_WEIGHT){
-            charge += OVER_WEIGHT_TAX;
-        }
-        else if(weight > LIGHT_WEIGHT_CUT){
-            charge += MIDDLE_WEIGHT_TAX;
-        }
-        else if(weight < LIGHT_WEIGHT_CUT){
-            charge += LIGHT_WEIGHT_TAX;
-        }
-
-
-        if (price >= FREE_DELIVERY_PEE_CUT){
-            charge = 0;
-        }
-        else if(weight > DISCOUNT_DELIVERY_PEE_CUT){
-            charge -= DISCOUNT_PEE;
-        }
-        return charge;
+    public int getDeliveryCharge(int price, int weight) {
+        return calculateTotalFee(price,weight);
     }
 
-    public int getWeight(){
-        return this.weight;
-    }
-
-    public void setWeight(int weight){
-        this.weight = weight;
-    }
-
-    public int getPrice(){
-        return this.price;
-    }
-
-    public void setPrice(int Price){
-        this.price = price;
+    public void Output(){
+        int DeliveryFee = getDeliveryCharge(this.price, this.weight);
+        System.out.println(this.name + "의 배송료는" + DeliveryFee + "원 입니다."
+                + "총 가격은" + (DeliveryFee + this.price) + "원 입니다.");
     }
 }
--- src/Grocery.java
+++ src/Grocery.java
@@ -1,7 +1,17 @@
 public class Grocery extends Product implements DeliveryChargeCalculator{
 
+    Grocery(String name, int price, int weight){
+        super(name, price, weight);
+    }
+
     @Override
-    public int getDeliveryCharge(int weight, int price) {
-        return 0;
+    public int getDeliveryCharge(int price, int weight) {
+        return calculateTotalFee(price,weight);
+    }
+
+    public void Output(){
+        int DeliveryFee = getDeliveryCharge(this.price, this.weight);
+        System.out.println(this.name + "의 배송료는" + DeliveryFee + "원 입니다."
+                + "총 가격은" + (DeliveryFee + this.price) + "원 입니다.");
     }
 }
