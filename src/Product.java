76a13f7e047e1d7e43671412688de4e52e2e3d09
dragon0622 <iamdragon.dev@gmail.com>
2024-02-03 00:49:44 +0900

    Feat : 무게대비 배송료, 가격대비 배송료 메소드 추가

--- src/Product.java
+++ src/Product.java
@@ -1,5 +1,34 @@
 public class Product {
-    private String name;
-    private int price;
-    private int weight;
+    public String name;
+    public int price;
+    public int weight;
+
+    Product(String name, int price, int weight){
+        this.name = name;
+        this.price = price;
+        this.weight = weight;
+    }
+    public int calculateTotalFee(int price, int weight){
+        if (price >= MagicNum.FREE_DELIVERY_FEE_CUT){
+            return 0;
+        }
+        else if(price > MagicNum.DISCOUNT_DELIVERY_FEE_CUT){
+            return calculateWeightFeeFirst(weight) - MagicNum.DISCOUNT_FEE;
+        }
+        else{
+            return calculateWeightFeeFirst(weight);
+        }
+    }
+
+    public int calculateWeightFeeFirst(int weight){
+        if (weight >= MagicNum.OVER_WEIGHT){
+            return MagicNum.OVER_WEIGHT_TAX;
+        }
+        else if(weight > MagicNum.LIGHT_WEIGHT_CUT){
+            return MagicNum.MIDDLE_WEIGHT_TAX;
+        }
+        else {
+            return MagicNum.LIGHT_WEIGHT_TAX;
+        }
+    }
 }
