cb382e252cbc041f9f8c3da0dbe0a717532acf77
dragon0622 <iamdragon.dev@gmail.com>
2024-02-03 00:49:59 +0900

    Feat : 객체 생성 및 예제 출력

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
 public class Main {
     public static void main(String[] args) {
 
+        Beauty beauty = new Beauty("화장품", 20000, 1);
+        Grocery grocery1 = new Grocery("빵", 15000, 5);
+        Grocery grocery2 = new Grocery("책", 45000, 15);
+        LargeAppliance largeAppliance = new LargeAppliance("냉장고", 250000, 100);
 
+        beauty.Output();
+        grocery1.Output();
+        grocery2.Output();
+        largeAppliance.Output();
 
     }
 }
