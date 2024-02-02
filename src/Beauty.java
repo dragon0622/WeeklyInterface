2aa839a5013375724d442e2ef5a1a6d47ab9a285
dragon0622 <iamdragon.dev@gmail.com>
2024-02-02 19:35:10 +0900

    Feat : 매직넘버 수정

--- src/Beauty.java
+++ src/Beauty.java
@@ -1,28 +1,40 @@
 public class Beauty extends Product implements DeliveryChargeCalculator{
 
+    final int OVER_WEIGHT = 10;
+    final int LIGHT_WEIGHT_CUT = 3;
+    final int OVER_WEIGHT_TAX = 10000;
+    final int MIDDLE_WEIGHT_TAX = 5000;
+    final int LIGHT_WEIGHT_TAX = 1000;
+
+    final int FREE_DELIVERY_PEE_CUT = 100000;
+    final int DISCOUNT_DELIVERY_PEE_CUT = 30000;
+    final int DISCOUNT_PEE = 1000;
+
     private int weight;
     private int price;
+
+
 
     @Override
     public int getDeliveryCharge(int weight, int price) {
         int charge = 0;
-        if (weight >= 10){
-            charge += 10000;
+        if (weight >= OVER_WEIGHT){
+            charge += OVER_WEIGHT_TAX;
         }
-        else if(weight > 3){
-            charge += 5000;
+        else if(weight > LIGHT_WEIGHT_CUT){
+            charge += MIDDLE_WEIGHT_TAX;
         }
-        else if(weight < 3){
-            charge += 1000;
+        else if(weight < LIGHT_WEIGHT_CUT){
+            charge += LIGHT_WEIGHT_TAX;
         }
 
-        if (price >= 100000){
+
+        if (price >= FREE_DELIVERY_PEE_CUT){
             charge = 0;
         }
-        else if(weight > 30000){
-            charge -= 1000;
+        else if(weight > DISCOUNT_DELIVERY_PEE_CUT){
+            charge -= DISCOUNT_PEE;
         }
-
         return charge;
     }
 
